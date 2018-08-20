ac0ca4088f750c31a4233ad4d353578f6dcb43c8
clap104 <claphh@gmail.com>
2018-08-20 04:35:22 -0400

    update port again

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import os
+
+port = int(os.environ.get('PORT', 5000))
 
 app = Flask(__name__)
 
@@ -19,5 +22,4 @@ def contact():
 	return render_template("contact.html")
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
