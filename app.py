f8a4df40db339a3e42b1d8fce5efbdb136de63ed
clap104 <claphh@gmail.com>
2018-08-20 00:46:39 -0400

    Update index

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET"])
+def homepage():
+	return render_template("index.html")
+
+@app.route('/how-it-work', methods = ["GET"])
+def howitwork():
+	return render_template("how-it-work.html")
+	
+@app.route('/services', methods = ["GET"])
+def services():
+	return render_template("services.html")
+	
+@app.route('/contact', methods = ["GET"])
+def contact():
+	return render_template("contact.html")
+
+if __name__ == '__main__':
+	app.run()
